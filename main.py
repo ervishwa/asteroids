0e6dccb65912dd7f0e27fa361803ab274e83dd05
ervishwa <vishwjeet@tealfeed.com>
2026-03-17 23:07:24 +0530

    refactor: limit fps

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0 
+
     while(True):
         log_state()
         for event in pygame.event.get():
@@ -16,6 +19,9 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        deltaTime = clock.tick(60)
+    # to convert millisecond to second
+        dt = deltaTime / 1000 
 
  
 if __name__ == "__main__":
